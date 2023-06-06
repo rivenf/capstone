fd3e29f980ec8f040e742a6612dca645ed35dbfe
Nico <rivenf@gmail.com>
2023-06-06 12:53:07 -0400

    added Formik for form handling

--- capstone/src/Components/BookingForm.js
+++ capstone/src/Components/BookingForm.js
@@ -1,24 +1,47 @@
-import React, {useState} from 'react';
+import React from 'react';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
 import '../App.css';
 
 export default function BookingForm() {
-    const [date, setDate] = useState('');
-    const [time, setTime] = useState('');
-    const [guests, setGuests] = useState('');
-    const [occasion, setOccasion] = useState('');
+
+    const formik = useFormik({
+        initialValues: {
+          date: '',
+          time: '',
+          guests: '',
+          occasion: '',
+          comment: '',
+        },
+        validationSchema: Yup.object({
+            date: Yup.date().required('Please enter a valid date'),
+            guests: Yup.number().required('Number of guests must be between 1 and 10').min(1).max(10),
+            comment: Yup.string(),
+          }),
+        onSubmit: values => {
+          console.log(values);
+        },
+      });
 
     return (
         <>
-            <form>
+            <form onSubmit={formik.handleSubmit}>
                 <label htmlFor="res-date">Choose date</label>
                 <input
                     type="date"
+                    name="date"
                     id="res-date"
-                    value={date}
-                    onChange={(e) => {setDate(e.target.value);}}
+                    {...formik.getFieldProps('date')}
                 />
-                <label htmlFor="res-time">Choose time</label>
-                <select id="res-time">
+                {formik.touched.date && formik.errors.date ?
+                    (<div>{formik.errors.date}</div>) : null}
+
+                <label htmlFor="time">Choose time</label>
+                <select
+                    id="time"
+                    name="time"
+                    {...formik.getFieldProps('time')}
+                >
                     <option>17:00</option>
                     <option>18:00</option>
                     <option>19:00</option>
@@ -26,21 +49,37 @@ export default function BookingForm() {
                     <option>21:00</option>
                     <option>22:00</option>
                 </select>
+
                 <label htmlFor="guests">Number of guests</label>
                 <input
                     type="number"
-                    placeholder="1"
-                    min="1" max="10"
                     id="guests"
-                    value={guests}
-                    onChange={(e) => {setGuests(e.target.value);}}
+                    name="guests"
+                    {...formik.getFieldProps('guests')}
                 />
+                {formik.touched.guests && formik.errors.guests ?
+                    (<div>{formik.errors.guests}</div>) : null}
+
                 <label htmlFor="occasion">Occasion</label>
-                <select id="occasion">
+                <select
+                    id="occasion"
+                    name="occasion"
+                    {...formik.getFieldProps('occasion')}
+                >
                     <option>Birthday</option>
                     <option>Anniversary</option>
+                    <option>Other</option>
                 </select>
-                <input type="submit" value="Make Your reservation" />
+
+                <label htmlFor="comment">Additional comments</label>
+                <input
+                    type="text"
+                    id="comment"
+                    name="comment"
+                    {...formik.getFieldProps('comment')}
+                />
+
+                <button type='submit'>Make a Reservation</button>
             </form>
         </>
     )
